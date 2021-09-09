b9b8a114dcfb07ef3dae37093105367ffbc3e587
Osvaldo Picazo <odpicazo@gmail.com>
2021-09-09 15:52:24 +0200

    project template v0

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
+require('dotenv').config()
+
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// Functional curling style of loading configuration
+require('./config/db')
+require('./config/global')(app)
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+const User = require("../models/User.model")
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  User.find().then((users)=>
+  res.render('index', { title: 'Express', users})
+  )
 });
 
 module.exports = router;
