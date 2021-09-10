5767cb809370023bd9a831babe47aa3c0171e7a3
FlavianAlbert <flahribu@gmail.com>
2021-09-10 12:05:49 +0200

    addet signup and login

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var express = require('express');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const authRouter = require("./routes/auth.routes");
+const privateRouter = require('./routes/private-routes');
 
 var app = express();
 
@@ -12,9 +14,12 @@ var app = express();
 require('./config/db')
 require('./config/global')(app)
 
+const isLoggedIn = require('./middleware/isLoggedIn');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter)
+app.use('/private', isLoggedIn, privateRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
